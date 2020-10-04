2e44a2b84ee7463fc2ea32ed183f58f8015b59c2
Robin Erickson <re@incuboweb.ca>
2020-10-04 12:15:12 -0400

    finished animation for us element and text

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 (() => {
     $(document).ready(() => {
-        const usBlock = $("#us");
-        const textPara1 = $(".text1");
-        const textPara2 = $(".text2");
-        const textPara3 = $(".text3");
-        const textPara4 = $(".text4");
+        const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-        usBlock.addClass("sliding-rotating");
+        const usBlock = $("#us");
+
+        const textBubble1 = $("#text-bubble-1");
+        const textBubble1Text = $("#text-1");
+
+        const textBubble2 = $("#text-bubble-2");
+        const textBubble2Text = $("#text-2");
+
+        const textBubble3 = $("#text-bubble-3");
+        const textBubble3Text = $("#text-3");
+
+        const textBubble4 = $("#text-bubble-4");
+        const textBubble4Text = $("#text-4");
+
+        usBlock.addClass("slide-right");
         usBlock[0].scrollIntoView();
 
         const interval = setInterval(() => {
@@ -18,45 +28,65 @@
             }
         }, 10);
 
-        const horizontalTime = 11000;
+        const timeUnit = 5000;
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "paused");
-            textPara1.text("Testing....");
+            textBubble1Text.css("animation-play-state", "paused");
+            textBubble2Text.css("animation-play-state", "paused");
+            textBubble3Text.css("animation-play-state", "paused");
+            textBubble4Text.css("animation-play-state", "paused");
+
+            textBubble1Text.text(lorem);
+            textBubble2Text.text(lorem);
+            textBubble3Text.text(lorem);
+            textBubble4Text.text(lorem);
         }, 0);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "running");
-        }, 3000);
+            textBubble1Text.css("animation-play-state", "running");
+        }, timeUnit);
+
+        setTimeout(() => {
+            textBubble2Text.css("animation-play-state", "running");
+        }, timeUnit * 2);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "paused");
-            usBlock.addClass("sliding");
-            usBlock.removeClass("sliding-rotating");
-        }, horizontalTime);
+            usBlock.addClass("slide-up");
+            usBlock.removeClass("slide-right");
+        }, timeUnit * 3);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "running");
-        }, horizontalTime + 1000);
+        }, timeUnit * 4);
+
+        setTimeout(() => {
+            textBubble3Text.css("animation-play-state", "running");
+        }, (timeUnit * 4) + 2000);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "paused");
+            usBlock.addClass("slide-left");
+            usBlock.removeClass("slide-up");
+
             clearInterval(interval);
             $("header")[0].scrollIntoView({
                 behavior: "smooth"
             });
-            textPara1.text("");
-        }, horizontalTime + 5000);
-
+        }, timeUnit * 5);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "running");
-            usBlock.addClass("sliding-rotating");
-            usBlock.removeClass("sliding");
-        }, horizontalTime + 6000);
+        }, timeUnit * 6);
+
+        setTimeout(() => {
+            textBubble4Text.css("animation-play-state", "running");
+        }, timeUnit * 7);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "paused");
-        }, horizontalTime + 14000);
+        }, (timeUnit * 8) + 500);
     });
 })();
