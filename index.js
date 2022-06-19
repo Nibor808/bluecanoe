6d62745051d94b964d17eef54a3272f830310e74
Robin Erickson <re@incuboweb.ca>
2022-06-19 10:17:03 -0400

    clean up and timing fixes

--- index.js
+++ index.js
@@ -1,76 +1,99 @@
+const fadeNode = (node, animation, time = "2s") => {
+    node.css("animation", `${animation} ${time} ease`);
+};
+
+const pauseAnimation = (node) => {
+    node.css("animation-play-state", "paused");
+};
+
+const startAnimation = (node) => {
+    node.css("animation-play-state", "running");
+};
+
+const setBackgroundToText = (bubble, container, text) => {
+    fadeNode(bubble, "fade-text-in", "5000ms");
+    container.css("margin-top", "160px");
+    bubble.text(text);
+    bubble.css("background", "");
+    bubble.css("height", "");
+};
+
+const setBackgroundToImg = (bubble, container, url) => {
+    pauseAnimation(bubble);
+    bubble.text("");
+    bubble.css("transition", "width ease");
+    bubble.css("width", "700px");
+    container.css("margin-top", "100px");
+
+    bubble.css("background", `url(${url}) no-repeat center`);
+    bubble.css("height", "300px");
+    fadeNode(bubble, "fade-image-in");
+    startAnimation(bubble);
+};
+
+const pauseAllAnimations = (usBlock, animationContainer) => {
+    usBlock.css("animation-play-state", "paused");
+    animationContainer.css("animation-play-state", "paused");
+};
+
+const startAllAnimations = (usBlock, animationContainer) => {
+    usBlock.css("animation-play-state", "running");
+    animationContainer.css("animation-play-state", "running");
+};
+
 (() => {
     $(document).ready(() => {
-        const firstText =
+        const text1 =
             "Hello! We're Jess and Robin and this is our blue canoe. During the pandemic in 2020 we decided to make more time for the out doors.";
+        const text2 = "On our first trip we brought a lot of things we didn't need!";
+        const text3 = "Eventually we started to figure it out.";
+        const text4 = "Follow us on our outdoor adventures in Sountern Ontario's Kawartha Lakes.";
 
         const usBlock = $("#us");
-        // const us2 = document.querySelector("#us");
         const animationContainer = $("#animation-container");
         const textContainer = $("#text-container");
-
         const textBubble = $("#text");
-        textBubble.css("animation", "fade-text-out 10s ease forwards");
 
-        usBlock.addClass("rotate");
-        animationContainer.addClass("slide-right");
+        usBlock.addClass("rotate"); // prevent any rotation on load by adding the class here
+        animationContainer.addClass("slide-right-half");
         usBlock[0].scrollIntoView();
 
-        const setBackgroundToImg = (bubble, url) => {
-            bubble.css("animation-play-state", "paused");
-            bubble.text("");
-            bubble.css("transition", "width ease");
-            bubble.css("width", "700px");
-            textContainer.css("margin-top", "100px");
-
-            bubble.css("background", `url(${url}) no-repeat center`);
-            bubble.css("height", "300px");
-            bubble.css("animation", "fade-in-image 2s ease forwards");
-            bubble.css("animation-play-state", "running");
-        };
-
-        // const fadeImg = (bubble, animation) => {
-        //     bubble.css("animation", `${animation} 2s ease forwards`);
-        // };
-
-        const setBackgroundToText = (bubble, text) => {
-            bubble.css("animation", "fade-text-in 5s ease forwards");
-            bubble.css("animation-play-state", "");
-            textContainer.css("margin-top", "160px");
-            bubble.text(text);
-            bubble.css("background", "");
-            bubble.css("height", "");
-        };
-
-        // const interval = setInterval(() => {
-        //     const us = us2.getBoundingClientRect();
-        //     const windowWidth = $(window).innerWidth();
-
-        //     if (us.right > windowWidth) {
-        //         usBlock[0].scrollIntoView();
-        //     }
-        // }, 10);
-
-        const timeUnit = 5000;
+        const timeUnit = 5500;
 
         setTimeout(() => {
-            usBlock.css("animation-play-state", "paused");
-            animationContainer.css("animation-play-state", "paused");
-            setBackgroundToText(textBubble, firstText);
+            pauseAllAnimations(usBlock, animationContainer);
+            setBackgroundToText(textBubble, textContainer, text1);
         }, 0);
 
         setTimeout(() => {
-            usBlock.css("animation-play-state", "running");
-            animationContainer.css("animation-play-state", "running");
-            setBackgroundToImg(textBubble, "./canoe.jpeg");
+            fadeNode(textBubble, "fade-text-out", "5500ms"); // add 500ms to cover next fade in
         }, timeUnit);
+
+        setTimeout(() => setBackgroundToText(textBubble, textContainer, text2), timeUnit * 2);
+
+        setTimeout(() => {
+            startAllAnimations(usBlock, animationContainer);
+            setBackgroundToImg(textBubble, textContainer, "./canoe.jpeg");
+        }, timeUnit * 3);
+
+        setTimeout(() => fadeNode(textBubble, "fade-image-out"), timeUnit * 3.8);
 
         setTimeout(() => {
             usBlock.css("animation-play-state", "paused");
-        }, timeUnit * 2);
+            setBackgroundToText(textBubble, textContainer, text3);
+        }, timeUnit * 4);
 
         setTimeout(() => {
+            animationContainer.removeClass("slide-right-half");
+            animationContainer.addClass("slide-right-full");
             usBlock.css("animation-play-state", "running");
             animationContainer.css("animation-play-state", "running");
-        }, timeUnit);
+            setBackgroundToImg(textBubble, textContainer, "./canoe.jpeg"); // add second canoe image - better pack
+        }, timeUnit * 4.5);
+
+        setTimeout(() => {
+            pauseAllAnimations(usBlock, animationContainer);
+            setBackgroundToText(textBubble, textContainer, text4);
+        }, timeUnit * 5.4);
     });
 })();
